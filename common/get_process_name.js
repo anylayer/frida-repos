801787183dada3debf936dcc2a060345f8b0b39b
FreaxW <46556817+FreaxW@users.noreply.github.com>
2021-08-04 10:01:08 +0800

    Create get_process_name.js
    get process name

---
+++ common/get_process_name.js
@@ -0,0 +1,17 @@
+function get_process_name(){
+    var openPtr = Module.getExportByName('libc.so', 'open');
+    var readPtr = Module.getExportByName('libc.so', 'read');
+    var closePtr = Module.getExportByName('libc.so', 'close');
+                                         
+    var open = new NativeFunction(openPtr, 'int', ['pointer', 'int']);
+    var read = new NativeFunction(readPtr, 'int', ['int', 'pointer', 'int']);
+    var close = new NativeFunction(closePtr, 'int', ['int']);
+    
+    var cmdline = Memory.allocUtf8String('/proc/self/cmdline');
+    var fd = open(cmdline, 0);
+    if (fd != -1){
+        var buffer = Memory.alloc(0x1000);
+        read(0, fd, buffer);
+        var result = ptr(buffer).readCString();
+        return result;
+}
